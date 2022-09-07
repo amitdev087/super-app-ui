75ff2af99847b5f7e6dc4ded95c56e73fb0fb3f2
achal1304 <achal20000@gmail.com>
2022-09-07 13:59:10 +0530

    performed get request and displayed list of customers

--- src/components/ListCustomers.js
+++ src/components/ListCustomers.js
@@ -7,89 +7,65 @@ export default class ListCustomers extends Component {
     constructor(props) {
         super(props);
 
-        // Initializing the state 
-        this.state = { color: 'lightgreen' };
+        this.state = {
+            customerslist: []
+        }
+        this.makeRequest = this.makeRequest.bind(this);
     }
-
-    componentDidMount() {
-        this.makeRequest()
-    }
-    makeRequest() {
-        // const CryptoJS = require("crypto-js");
-
-        const salt = CryptoJS.lib.WordArray.random(12); // Randomly generated for each request.
-        const timestamp = (Math.floor(new Date().getTime() / 1000) - 10).toString(); // Current Unix time (seconds).
-        const access_key = "94FBABD5DA7F959BC979"; // The access key from Client Portal.
-        const secret_key = "672d23cd5b3c8602c472bceaab2d17323c6ac92e13a61e0e8d099756f622994cb394f3a1f2de7d5e"; // Never transmit the secret key by itself.
-        const url_path = "/v1/data/countries"; // Portion after the base URL.
-        const baseURL = "https://sandboxapi.rapyd.net";
-        // Hardkeyed for this example.
-        const http_method = "get"; // get|put|post|delete - must be lowercase.
-        const data = ""; // Stringified JSON without whitespace.
-        // Always empty string for GET;
-
-        console.log(salt)
-        const getSignature = () => {
-            const to_sign =
-                http_method +
-                url_path +
-                salt +
-                timestamp +
-                access_key +
-                secret_key +
-                data;
-            let signature = CryptoJS.enc.Hex.stringify(
-                CryptoJS.HmacSHA256(to_sign, secret_key)
-            );
-
-            signature = CryptoJS.enc.Base64.stringify(
-                CryptoJS.enc.Utf8.parse(signature)
-            );
-
-            return signature;
+    async makeRequest() {
+        const headers = {
+            "Content-Type": `application/json`
         };
 
-        var idempotency = new Date().getTime().toString();
-        var signature = getSignature()
-        const headers = {        
-            "Content-Type": `application/json`,
-            "access_key": access_key,
-            "signature": signature,
-            // "salt": salt.toString(),
-            // "timestamp": timestamp,
-            // "idempotency" : idempotency,
-            "Access-Control-Allow-Headers":"salt",  
-            "Access-Control-Allow-Headers":"*"
+        const request = {
+            baseURL: "http://127.0.0.1:8000/countries/",
+            headers,
+            method: 'get',
         };
 
-        console.log(headers)
-
-        //   const request = {
-        //     baseURL: "https://sandboxapi.rapyd.net",
-        //     headers,
-        //     url: url_path,
-        //     method: http_method,
-        //     data,
-        //   };
-        // fetch(baseURL + url_path, {
-        //     headers: headers,
-        //     method: "get"
-        // })
-        //     .then((response) => response.json())
-        //     .then((data) => console.log(data));
-        var finURL = baseURL + url_path
-        axios.get(finURL, {
-            headers: headers
-        }).then((response) => {
-            // setPost(response.data);
-            console.log(response.data)
+        // You can use any HTTP request library to make the request. Example: Axios
+        const response = await axios(request);
+        console.log(response.data)
+        // for (var ele in response.data['data']){
+        //     var customer = ele
+        //     this.state.customerslist.push(customer)
+        // }
+        var responsecustomers = []
+        response.data['data'].forEach(element => {
+            var customer = element
+            // console.log(responsecustomers)
+            if (customer.ewallet != "") {
+                responsecustomers.push(customer)
+            }
         });
+
+        this.setState({ customerslist: responsecustomers }, () => {
+            console.log("csutomers = ", this.state.customerslist)
+        })
     }
 
 
     render() {
+        console.log(this.state.customerslist);
         return (
-            <div>ListCustomers</div>
+            <div>
+                {/* <div>{this.state.customerslist}</div> */}
+                <div>ListCustomers</div>
+                <button onClick={this.makeRequest} label="Get customers">Get customers</button>
+                <ul>
+                    {this.state.customerslist.map((customer) => {
+                        console.log(customer);
+                        return (
+                            <div>
+                                <button key={customer.id}>
+                                    Name : {customer.name}
+                                </button>
+                            </div>
+                        )
+                    })}
+                </ul>
+
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -9,15 +9,14 @@ import ListCustomers from './components/ListCustomers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Make_payment/>
+  // <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={Make_payment}></Route>
+        <Route path="/" element={<Make_payment/>}></Route>
         <Route path="/listcustomers" element={<ListCustomers/>}></Route>
       </Routes>
     </Router>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
