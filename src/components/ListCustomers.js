b17b1fa2b6a11e4f4643ee37bd66ed4a29be427a
achal1304 <achal20000@gmail.com>
2022-09-11 21:37:22 +0530

    pay for friend remaining

--- src/components/ListCustomers.js
+++ src/components/ListCustomers.js
@@ -5,6 +5,8 @@ import axios from "axios";
 import 'react-bootstrap';
 import SplitIt from './splitIt';
 import { Link } from 'react-router-dom'
+import '../styles/splitpay.css'
+import ReactSpinnerTimer from "react-spinner-timer";
 
 export default class ListCustomers extends Component {
     constructor(props) {
@@ -16,8 +18,15 @@ export default class ListCustomers extends Component {
             merchantsList: [],
             selectedMerchant: "",
             showModalPopup: false,
-            pendingResponse: {}
+            pendingResponse: {},
+            isGpCompleted: false,
+            gpList: [],
+            showSpinner: false,
+            isFailedUserInList: false
         }
+
+        this.FailedUser = "cus_616804c7789f0342bd7664a5fa78f3b9";
+
         this.makeRequest = this.makeRequest.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.createGroup = this.createGroup.bind(this);
@@ -26,13 +35,30 @@ export default class ListCustomers extends Component {
         this.handleClickMerchant = this.handleClickMerchant.bind(this);
         this.makePaymentEwallet = this.makePaymentEwallet.bind(this);
         this.isShowPopup = this.isShowPopup.bind(this);
+        this.handleLapChange = this.handleLapChange.bind(this);
     }
 
     isShowPopup = (status) => {
         this.setState({ showModalPopup: status });
     };
     async componentDidMount() {
+
         await this.makeMerchantRequest();
+        await this.makeRequest();
+    }
+    async componentDidUpdate() {
+        for (var x in this.state.selectedIds) {
+            if (this.state.selectedIds[x] == this.FailedUser) {
+                this.setState({
+                    isFailedUserInList: true
+                })
+            }
+            else{
+                this.setState({
+                    isFailedUserInList: false
+                })
+            }
+        }
     }
 
     async makeMerchantRequest() {
@@ -87,11 +113,18 @@ export default class ListCustomers extends Component {
     }
 
     createGroup = async () => {
+
         var totalCustomerCount = this.state.selectedIds.length + 1;
-        var finalamount = this.state.amount - ((this.state.amount) / (totalCustomerCount))
+        if (this.state.isFailedUserInList) {
+            var finalamount = this.state.amount - 2*((this.state.amount) / (totalCustomerCount))
+        }
+        else {
+            var finalamount = this.state.amount - ((this.state.amount) / (totalCustomerCount))
+        }
+
         var gpbody = {
-            amount: finalamount.toString(),
-            ids: this.state.selectedIds
+            amount: finalamount,
+            ids: this.state.selectedIds.filter((x) => x != this.FailedUser)
         }
 
         console.log("finalbody is ", gpbody);
@@ -107,7 +140,29 @@ export default class ListCustomers extends Component {
         };
 
         const response = await axios(request);
-        console.log(response.data)
+        var gpMembers = []
+        for (var ele in this.state.customerslist) {
+            for (var x in this.state.selectedIds) {
+                if (this.state.customerslist[ele].id == this.state.selectedIds[x]) {
+                    gpMembers.push(this.state.customerslist[ele])
+                }
+            }
+        }
+
+        console.log("selectedIds are ************** ", this.state.selectedIds)
+        if (response.status == 200 || response.status == 201) {
+            this.setState({
+                isGpCompleted: true,
+                gpList: gpMembers,
+                showSpinner: true
+            }, () => { console.log("gpbody is *******", gpMembers) })
+        }
+        else {
+            this.setState({
+                isGpCompleted: false,
+                isFailedUserInList: false
+            })
+        }
     }
 
 
@@ -161,8 +216,17 @@ export default class ListCustomers extends Component {
         console.log('handleClick 👉️', this.state.amount);
     }
 
+    handleLapChange = (lap) => {
+        if (lap.isFinish)
+            this.setState({
+                showSpinner: false
+            })
+        else
+            console.log("Running!! Lap:", lap.actualLap);
+    };
+
     handleClickMerchant = (e) => {
-        e.preventDefault();
+        // e.preventDefault();
         console.log(e.target.value);
         this.setState({
             selectedMerchant: e.target.value
@@ -174,11 +238,14 @@ export default class ListCustomers extends Component {
         let merchants;
         let clickedMerchant;
         let makePaymentEwallet;
-        if (this.state.selectedMerchant == "") {
-            clickedMerchant = "";
-            console.log("inside render for merhcant ", this.state.merchantsList);
-            merchants = <ul>
-                {this.state.merchantsList.map((merchant) => <label key={merchant.ewallet}>
+        let createdGPWIthLoader = "";
+        let isMerchantSelected = this.state.selectedMerchant == "" ? false : true;
+        // if (this.state.selectedMerchant == "") {
+        clickedMerchant = "";
+        console.log("inside render for merhcant ", this.state.merchantsList);
+        merchants = <div className='transaction_list_wrapper'><h5>Select Merchant</h5><hr></hr>
+            <ul className='grid_listcustomers' style={{ padding: "5px" }}>
+                {this.state.merchantsList.map((merchant) => <label style={{ textAlign: "center" }} key={merchant.ewallet}>
                     <input
                         type="radio"
                         name="lang"
@@ -186,32 +253,56 @@ export default class ListCustomers extends Component {
                         onChange={this.handleClickMerchant}
                     /> {merchant.name}
                 </label>)}
-            </ul>
-        }
-        else {
-            clickedMerchant = <div>
-                <input
-                    type="number"
-                    id="amount"
-                    name="amount"
-                    onChange={this.handleClickAmount}
-                    autoComplete="off"
-                />
+            </ul></div>
 
-                <h2>Amount: {this.state.amount}</h2>
+        // }
+        // else {
+        clickedMerchant = <div className='form-group'>
+            <input
+                type="number"
+                id="amount"
+                name="amount"
+                onChange={this.handleClickAmount}
+                autoComplete="off"
+                className="form-control"
+                placeholder='Enter Amount'
+            />
+        </div>
+
+        if (this.state.isGpCompleted) {
+            createdGPWIthLoader = <div className='transaction_list_wrapper'><h5>Group Payment Status</h5><hr></hr>
+                <ul className='display_groupStatus'>
+                    {this.state.gpList.map((customer) => <label key={customer.id}>
+                        <div>
+                            <h6 value={customer.id}
+                            > {customer.name}</h6>
+                            {this.state.showSpinner ? <div>
+                                <ReactSpinnerTimer
+                                    timeInSeconds={5}
+                                    totalLaps={1}
+                                    isRefresh={false}
+                                    onLapInteraction={this.handleLapChange}
+                                    isPause={false}
+                                />
+                            </div> : <h6> {this.FailedUser == customer.id ? "Failed" : "Passed"}</h6>}
+
+
+
+                        </div>
+                    </label>)}
+                </ul>
             </div>
         }
-
         let button;
         if (this.state.selectedIds.length > 0) {
-            button = <button onClick={this.createGroup}> Create Group </button>;
+            button = <button className='button_primary' onClick={this.createGroup} disabled={!isMerchantSelected}> {isMerchantSelected ? "Create Group Payment" : "Please select merchant"} </button>;
         }
         console.log("button value is :", this.state.selectedIds > 0);
         let finalamount;
         if (this.state.amount > 0) {
-            finalamount = <div><div>ListCustomers</div>
-                <button onClick={this.makeRequest} label="Get customers">Get customers</button>
-                <ul>
+            finalamount = <div className='transaction_list_wrapper'><h5>Select Friends</h5><hr></hr>
+                {/* <button onClick={this.makeRequest} label="Get customers">Get list of Friends</button> */}
+                <ul className='grid_listcustomers'>
                     {this.state.customerslist.map((customer) => <label key={customer.id}>
                         <input
                             type="checkbox"
@@ -225,15 +316,16 @@ export default class ListCustomers extends Component {
             </div>
         }
         if (this.state.amount > 0 && this.state.selectedIds.length > 0 && this.state.selectedMerchant != "") {
-            makePaymentEwallet = <button onClick={this.makePaymentEwallet} label="Make Payment to Merchant">Make Payment to Merchant</button>
+            makePaymentEwallet = <button className='button_primary' onClick={this.makePaymentEwallet} label="Make Payment to Merchant">Make Payment to Merchant</button>
         }
         return (
-            <div>
+            <div className='container' style={{ padding: '2rem', width: '60%' }}>
                 <div>
                     {merchants}
                     {clickedMerchant}
                     {finalamount}
                     {makePaymentEwallet}
+                    {createdGPWIthLoader}
 
                 </div>
                 <div>
@@ -244,7 +336,7 @@ export default class ListCustomers extends Component {
                                 <div
                                     className="nav-item"
                                     onClick={() => this.isShowPopup(true)}>
-                                    <button>Modal Pop up</button>
+                                    <button className='button_primary'>Modal Pop up</button>
                                 </div>
                             </Fragment>
                         </header>
@@ -255,19 +347,22 @@ export default class ListCustomers extends Component {
                         ></ModalPopup>
                     </Fragment>
                 </div>
-                <div>
-                    <Link to={{
-                        pathname: "/splitIt"
-                    }}>
-                        Go to SplitIt
-                    </Link>
-                </div>
-                <div>
-                    <Link to={{
-                        pathname: "/salarySplit"
-                    }}>
-                        Go to Salary Split
-                    </Link>
+                <div style={{ display: 'flex', padding: "5px 0" }}>
+                    <button className='button_primary' style={{ width: "95%" }}>
+                        <Link to={{
+                            pathname: "/splitIt"
+                        }}>
+                            Go to SplitIt
+                        </Link>
+                    </button>
+                    {/* </div>
+                <div> */}
+                    <button className='button_primary' style={{ width: "95%" }}>
+                        <Link to={{
+                            pathname: "/salarySplit"
+                        }}>
+                            Go to Salary Split
+                        </Link></button>
                 </div>
             </div>
         )
