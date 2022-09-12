0a23678ca314ffa0682919a2a354d6775d32c607
gopheramit <amitwpu@gmail.com>
2022-09-12 22:08:18 +0530

    enable disable complete

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Routes, Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import Make_payment from './components/MakePayments';
-import ListCustomers from './components/ListCustomers';
-import SplitIt from './components/splitIt';
-import SalarySplit from './components/SalarySplit';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import TransactionReducer from './Redux/Reducer/TransactionReducer';
-import {createStore} from 'redux'
-import allReducers from './Redux/Reducer/CombinedReducer';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import Make_payment from "./components/MakePayments";
+import ListCustomers from "./components/ListCustomers";
+import SplitIt from "./components/splitIt";
+import SalarySplit from "./components/SalarySplit";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import TransactionReducer from "./Redux/Reducer/TransactionReducer";
+import { createStore } from "redux";
+import allReducers from "./Redux/Reducer/CombinedReducer";
 
 const store = createStore(allReducers);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-  // <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Make_payment />}></Route>
-          <Route path="/listcustomers" element={<ListCustomers />}></Route>
-          <Route path="/splitIt" element={<SplitIt />}></Route>
-          <Route path="/salarySplit" element={<SalarySplit />}></Route>
-        </Routes>
-      </Router>
-  // </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Make_payment />}></Route>
+        <Route path="/listcustomers" element={<ListCustomers />}></Route>
+        <Route path="/splitIt" element={<SplitIt />}></Route>
+        <Route path="/salarySplit" element={<SalarySplit />}></Route>
+      </Routes>
+    </Router>
   </Provider>
 );
 
