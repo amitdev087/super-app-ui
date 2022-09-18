c8cd1869c739323c3b669155cce31cb1649140cf
achal1304 <achal20000@gmail.com>
2022-09-18 11:22:38 +0530

    added the name at the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/header.css";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Header() {
   const [isLoggedOut, setIsLoggedOut] = useState(false);
   const [isUserAvailable, setIsUserAvailable] = useState("");
+  const [custId, setCustId] = useState("");
+  const [customerName, setCustomerName] = useState("");
   const logout = async () => {
     await localStorage.clear()
-    var custId = await localStorage.getItem('custId')
     setIsLoggedOut(true);
   }
+  useEffect(()=>{
+    var customer = localStorage.getItem('custId')
+    setCustId(customer);
+    if(customer != null || customer != "" || customer != undefined){
+      userName(customer).then((custname)=>{
+        setCustomerName(custname);
+      });
+      
+    }
+  })
+
+  const userName = async (custId) => {
+    var finalBody = {
+      custId : custId
+    };
+
+    const headers = {
+      "Content-Type": `application/json`,
+    };
+
+    const request = {
+      baseURL: "http://127.0.0.1:8000/getName/",
+      headers,
+      data: finalBody,
+      method: "post",
+    };
+    const response = await axios(request);
+    if (response.status = 201 && response.data != -1) {
+      return response.data
+    }
+    return ""
+  };
   return (
     <div class="topnav">
+      
+      <a class="active" href="/splitPay">
+        {(custId != null || custId != "" || custId != undefined) && customerName != "" ? <div> Hi {customerName} </div>: <div></div>}
+      </a>
+
       <a class="active" href="/splitPay">
         Split Pay
       </a>
-
+      
       <a href="/splitIt">Split It</a>
 
       <a href="/salarySplit"> Manage Salary</a>
-      <a href="/lend Money"> Lend Money</a>
+      <a href="/lent"> Lend Money</a>
 
       <a href="/login"  style={{ float: "right" }} onClick={logout}> Logout</a>
       {isLoggedOut ? <Navigate replace to="/login" /> : <div></div>}
